a1653005246979a5a5e8097acfda92d853ac7b0f
takaiten <o1smol99@gmail.com>
2019-06-28 23:15:20 +0700

    canvas depends on windows height

--- source/sketch.js
+++ source/sketch.js
@@ -1,14 +1,10 @@
 let ants;
 
 function setup() {
-    // const res = window.innerHeight - 100;
     createCanvas(window.innerHeight + 100, window.innerHeight);
-    // ants = new LangtonsAnt(50, 60, 'LLRLR');
-    // ants.changeColors('blue', 'red', 'yellow', 'magenta', 'gray');
 
     ants = new LangtonsAnt(50, window.innerHeight - 100, 60, 'RLLLRLLL');
     ants.drawSequence();
-    // ants.changeBehavior("RLLLR"); // RLLLR - kinda square after 130k steps
 }
 
 function draw() {
